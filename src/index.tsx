5475df3dc8687015f7424f6ed986652deffba2ed
Tran Cong Viet An <trancongvietan22@gmail.com>
2023-10-01 20:11:41 +0700

    feat: patch dev server for hot reloading

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,53 @@
 import "vite/modulepreload-polyfill";
-import register from "preact-custom-element";
-import Input from "./Input";
-const MentionInput = (props) => {
+import {
+  BubblePreactComponent,
+  registerComponent,
+  useBubbleState,
+} from "@citizendev/bubble-preact-base";
+
+// Fill in the shapes of the Bubble element fields here
+type DemoFields = {
+  name: string;
+};
+
+// Fill in the shapes of the Bubble element states here
+type DemoStates = {
+  value: string;
+  focused: boolean;
+};
+
+// Events should be a union type of strings, for example: "event1" | "event2"
+type DemoEvents = "changed" | "focused";
+
+const DemoElement: BubblePreactComponent<
+  DemoFields,
+  DemoStates,
+  DemoEvents
+> = ({ instance, bubbleContext, properties }) => {
+  const [value, setValue] = useBubbleState(instance, "value", "");
+  const [focused, setFocused] = useBubbleState(instance, "focused", false);
+
+  const handleChange = (event: React.TargetedEvent<HTMLInputElement>) => {
+    let target = event.target as HTMLInputElement;
+    setValue(target.value);
+    instance?.triggerEvent?.("changed");
+  };
+
   return (
     <div>
-      <Input />
+      <div>Hello</div>
+      <input
+        onChange={handleChange}
+        onFocus={() => {
+          setFocused(true);
+          instance?.triggerEvent("focused");
+        }}
+        onBlur={() => setFocused(false)}
+      />
     </div>
   );
 };
 
-export default MentionInput;
+export default DemoElement;
 
-if (!customElements.get("mentions-input")) {
-  register(MentionInput, "mentions-input", [
-    "properties",
-    "instance",
-    "bubbleContext",
-  ]);
-}
+registerComponent(DemoElement, "mentions-input");
--- vite.config.ts
+++ vite.config.ts
@@ -1,52 +1,14 @@
+import preactBubblePlugin from "@citizendev/vite-plugin-preact-bubble";
 import preact from "@preact/preset-vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
-    preact({
+    preactBubblePlugin({
       include: "**/*.tsx",
+      bubble: {
+        libraryName: "CDMentionsInput"
+      }
     }),
   ],
-  server: { hmr: true, host: "localhost" },
-  css: {
-    modules: {
-      localsConvention: "camelCase",
-    },
-  },
-  build: {
-    manifest: true,
-    sourcemap: true,
-    rollupOptions: {
-      input: "src/index.ts",
-      external: [
-        "preact",
-        "preact-custom-element",
-        "preact/compat",
-        "preact/hooks",
-      ],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          preact: "preact",
-          "preact-custom-element": "preactCustomElement",
-          "preact/compat": "preactCompat",
-          "preact/hooks": "preactHooks",
-        },
-      },
-      onwarn(warning, warn) {
-        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
-          return;
-        }
-        warn(warning);
-      },
-    },
-    outDir: "dist",
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: "src/index",
-      fileName: "index",
-      name: "CZTimePickerPlugin",
-    },
-  },
 });
